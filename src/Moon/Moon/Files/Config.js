23f73257adf97798985a930466f974d238ab93e4
mounir labaied <mounir.labaied@ms-exchanger.com>
2018-07-24 21:56:29 +0200

    c

---
+++ src/Moon/Moon/Files/Config.js
@@ -0,0 +1,9 @@
+﻿{
+    "Config"[
+        { "Vars_ShouldBuy": false },
+        { "Vars_ShouldSell": false },
+        { "Vars_ShouldMonitor": false },
+        { "Vars_ShouldCollectAndArchive": false },
+        { "Vars_ShouldNNodes": false }
+    ]
+}
