058d52d123731573d18db08c7cf9dfcd7c4ba107
mounir labaied <mounir.labaied@ms-exchanger.com>
2018-07-25 20:19:27 +0200

    b

--- src/Moon/Moon/Finance/Risk/Core.cs
+++ src/Moon/Moon/Finance/Risk/Core.cs
@@ -9,4 +9,49 @@ namespace Moon.Finance.Risk
     class Core
     {
     }
+
+   
+
+    class Identity
+    {
+        class Objectives
+        {
+            public RiskGroups Group { get; set; }
+            public RiskTable Table { get; set; }
+            public Experiences Exp { get; set; }
+            public WBS WorkBreakDownStructure { get; set; }
+
+        }
+        class RiskGroups { }
+        class RiskTable { }
+        class Experiences { }
+        class WBS {
+            public string[] DirectL1 { get; set; }
+            public string[] DirectL3 { get; set; }
+            public string[] DirectL4 { get; set; }
+            public string[] DirectL5 { get; set; }
+            public bool MatchTree { get; set; }
+            public bool HasDirectTimeImpact { get; set; }
+            public TimeSpan RoT { get; set; }
+        }
+
+
+    }
+    class Classify
+    {
+
+    }
+    class Quantify
+    {
+
+    }
+    class Grade
+    {
+
+    }
+    class Manage
+    {
+
+    }
 }
+    
