6c95a24dbf3358c2095cf5412eb80d59d87290e3
mounir labaied <mounir.labaied@ms-exchanger.com>
2018-07-25 20:46:59 +0200

    e

--- src/Moon/Moon/Finance/Model/InvestmentManagement.cs
+++ src/Moon/Moon/Finance/Model/InvestmentManagement.cs
@@ -7,7 +7,24 @@ using System.Threading.Tasks;
 namespace Moon.Finance.Model.InvestmentManagement
 {
 
-        class PortofilioModelling { }
+        class PortofilioModelling {
+
+        // at least 3 currency not directly linked for passing an order (ETH/BTC/BNB ??)
+        public int cashflow_distribution = 5;
+        // at least x2 volume of movement on current pair
+        public int activity_median_volume_move = 2;
+        // allow portofilio pairs balances staying on overall downtrend
+        public bool allow_neg_keypairs = false;
+
+
+         internal bool IsAllowedToPark(string pairname)
+        {
+            return true;
+        }
+
+
+
+        }
         class InvestmentDecisions { }
         class StrategicAdvice { }
         class PortofilioConstruction { }
