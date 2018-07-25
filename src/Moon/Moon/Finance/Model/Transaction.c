2e2c5560ae6ae5e76e91d34ee5aaf866116de93e
mounir labaied <mounir.labaied@ms-exchanger.com>
2018-07-25 20:39:04 +0200

    d

---
+++ src/Moon/Moon/Finance/Model/Transaction.c
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moon.Finance.Model.Transaction
+{
+    class TransactionAndOrderExecution
+    {
+		//Orders - Transaction - Slipage mgmt
+    }
+	class TransactionSupportAndCurrencyMgmt
+	{ 
+		//Support and currency management
+	}
+	class PreTradeCompliance
+	{
+		//Pre entry compliance check rules
+	}
+}
