f84a9b52f83e1104092535b35600467f3ed25aba
mounir labaied <mounir.labaied@ms-exchanger.com>
2018-08-22 18:26:05 +0200

    Core TA

--- src/Moon/Moon/Finance/Indicators/Core.cs
+++ src/Moon/Moon/Finance/Indicators/Core.cs
@@ -8,6 +8,18 @@ namespace Moon.Finance.Indicators
 {
    public class Core
     {
+        public List<Type> ClassBase { get; set; }
+        public Moon.Data.Provider.Core provider { get; set; }
+        public List<RootTA> TA { get; set; }
+        public Core()
+        {
+            ClassBase.Add(new RSI().GetType());
+            TA.Add(new RSI());
 
+        }
+        public void LinkMeToo(Moon.Data.Provider.Core core)
+        {
+
+        }
     }
 }
