9d9ba980d295fd635731c44ac6c01387160b0e50
mounir labaied <mounir.labaied@ms-exchanger.com>
2018-09-09 14:00:34 +0200

    Sconf

--- src/Moon/Moon/Data/Model/IStrategyConf.cs
+++ src/Moon/Moon/Data/Model/IStrategyConf.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +20,47 @@ namespace Moon.Data.Model
     {
          DateTime GenerationDate { get; set; }
          StrategyConf Source { get; set; }
+
+    }
+    public class StrategyConfig : IRoot, IStrategyConf , INotifyPropertyChanged
+    {
+        private StrategyConf source = StrategyConf.FromAssembly;
+        public string Jscontainer { get; set; }
+        public string TypeOfData { get; set; } = "Strategy Config";
+        public DateTime GenerationDate { get; set; } = DateTime.Now;
+        public StrategyConf Source {
+            get { return this.source; }
+            set
+            {
+                if(value != this.source)
+                {
+                    this.source = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+        }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public StrategyConfig()
+        {
+            this.Jscontainer = Newtonsoft.Json.JsonConvert.SerializeObject(this);
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
 
     }
 }
